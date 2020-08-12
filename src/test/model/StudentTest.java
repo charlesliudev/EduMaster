f77f76ac2f25968b77cfe48f1a764e851eab413d
UBC Student <cliu205@students.cs.ubc.ca>
2020-08-11 19:33:47 -0700

    Update StudentTest.java

--- src/test/model/StudentTest.java
+++ src/test/model/StudentTest.java
@@ -34,13 +34,13 @@ class StudentTest {
 
     @Test
     public void testPayTuition() {
-        george.outstandingTuition = 1000;
-        assertEquals(george.outstandingTuition, 1000);
-        george.payTuition(300, mySchool);
-        assertEquals(george.outstandingTuition, 700);
+        george.outstandingTransaction = 1000;
+        assertEquals(george.getOutstandingTransaction(), 1000);
+        george.payOutstandingTransaction(300, mySchool);
+        assertEquals(george.outstandingTransaction, 700);
         assertEquals(mySchool.accumulatedAnnualTuition, 300);
         assertEquals(mySchool.transactionRecordSummary.size(), 1);
-        assertEquals(george.tuitionRecord.size(), 1);
+        assertEquals(george.transactionRecord.size(), 1);
     }
 
     @Test
@@ -59,14 +59,14 @@ class StudentTest {
 
     @Test
     public void testEnrollSameCourseTwice() {
-        assertEquals(george.coursesEnrolled.size(), 0);
+        assertEquals(george.courses.size(), 0);
         assertEquals(cpsc210.students.size(), 0);
         george.enroll(cpsc210);
         assertEquals(cpsc210.students.size(), 1);
-        assertEquals(george.coursesEnrolled.size(), 1);
+        assertEquals(george.courses.size(), 1);
         george.enroll(cpsc210);
         assertEquals(cpsc210.students.size(), 1);
-        assertEquals(george.coursesEnrolled.size(), 1);
+        assertEquals(george.courses.size(), 1);
     }
 
     @Test
